813b4d378d74ea99eae647ef42a23d5849037bb1
Muhammad Thariq 'Audah <thariq.audah@gmail.com>
2023-12-22 09:42:35 +0700

    modify vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const NODE_ENV = process.env.NODE_ENV;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	base:
+		NODE_ENV === 'production'
+			? '/gerakmuda-student-newsletter-react/'
+			: '/',
+});
